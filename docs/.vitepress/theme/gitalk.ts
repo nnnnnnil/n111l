ed01e46850df4b5b6647d18060526effa3a3fc13
nnnnnnil <13148656108@163.com>
2025-08-06 21:30:41 +0800

    🐞 fix: gitalk comment

--- docs/.vitepress/theme/gitalk.ts
+++ docs/.vitepress/theme/gitalk.ts
@@ -9,6 +9,9 @@ export default function initGitalk(path: string) {
     owner: 'nnnnnnil',
     admin: ['nnnnnnil'], // 仓库管理员，可初始化评论
     id: path, // 用于区分不同页面的唯一标识，通常使用路径名
+    labels: [ 'Gitalk' ],
+    // 如果 Issue 不存在，且登陆的是管理员账号，是否显示创建 Issue 按钮
+    createIssueManually: true,
     distractionFreeMode: false, // 是否启用无干扰模式
     language: 'zh-CN' // 语言
   })
